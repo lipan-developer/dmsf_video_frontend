1a0f3c3f638e7928582f53fdf00bfef6c85b47d8
李攀 <524789070@qq.com>
2018-12-13 18:01:12 +0800

    search something

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dmsfvideo';
+  searchValue = ''
+
+  getSeachResult(){
+    alert(this.searchValue)
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -39,7 +39,8 @@ import {VgBufferingModule} from 'videogular2/buffering';
     VgCoreModule,
     VgControlsModule,
     VgOverlayPlayModule,
-    VgBufferingModule
+    VgBufferingModule,
+    FormsModule
   ],
   providers: [HomeService
               ,AnimationService
